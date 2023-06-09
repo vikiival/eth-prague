e220b180114f6d6a0ba8fd01594887de1a7657a9
daiagi <daiagi@gmail.com>
2023-06-10 02:19:12 +0700

    explore page cards - start

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		domains: ['cryptologos.cc', 'ethereum.org', 'www.bitdegree.org', 'upload.wikimedia.org', 'assets.coingecko.com'],
+	  },
 }
 
 module.exports = nextConfig
