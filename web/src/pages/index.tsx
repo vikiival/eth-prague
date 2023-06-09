0682d7627b1ff69fc59d905b9737438f5ca595c9
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-06-09 21:20:50 +0200

    adding header

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react'
 import { APP_NAME } from '@/lib/consts'
 import Link from 'next/link';
+import Head from 'next/head'
+import Header from '@/components/Header'
+
 import ConnectWallet from '@/components/ConnectWallet'
 import { BookOpenIcon, CodeIcon, ShareIcon } from '@heroicons/react/outline'
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 const Home: FC = () => {
 	return (
-		<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+		<div className=" relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+			<Head>
+				<title>Medium Blog</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+
 			<div className="absolute top-6 right-6">
+				<Header />
 				<ConnectWallet />
 			</div>
 			<ThemeSwitcher className="absolute bottom-6 right-6" />
