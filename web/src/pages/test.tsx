79005d40dcc6c0b4c0526ef44f886b69769ad4b9
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-06-10 21:04:55 +0200

    fixed

--- web/src/pages/test.tsx
+++ web/src/pages/test.tsx
@@ -16,15 +16,15 @@ const Test: FC = () => {
           
         />
       </div>
-      <div className="flex flex-col max-w-2xl items-start">
+      <div className="flex flex-col max-w-2xl w-full items-start">
         <div className="text-base text-[#16392d] font-dela-gothic flex flex-col w-auto h-auto">
           Hallstatt article
         </div>
-        <div className="flex justify-between items-center w-auto h-auto">
-          <div className="flex items-center space-x-4 w-full h-auto">
+        <div className="flex justify-between items-center w-full h-auto">
+          <div className="flex items-center space-x-4">
             <div className="text-sm font-roboto text-[#16392d] font-bold w-auto h-auto whitespace-nowrap flex flex-col">
               Creator:
-              </div>
+            </div>
             <div className="flex items-center space-x-3 w-auto h-auto">
               <div className="w-5 h-5  flex items-center justify-center p-2.5 rounded-xl bg-center bg-no-repeat bg-cover overflow-hidden"
                 style={{backgroundImage: `url('https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg')`}}>
@@ -37,6 +37,7 @@ const Test: FC = () => {
           </div>
         </div>
       </div>
+
       <div className="flex flex-col max-w-2xl w-auto h-auto space-y-6">
           <div className="text-sm text-black font-roboto text-opacity-75">Lorem ipsum dolor sit amet consectetur. Congue risus sit elementum urna facilisis dignissim sem. Id morbi massa sed pretium commodo sit id. Risus auctor diam sed ornare maecenas risus diam integer. A aenean facilisis elit tincidunt imperdiet adipiscing turpis hendrerit.</div>
           <div className="mt-0 text-sm text-black font-roboto text-opacity-75"></div>
